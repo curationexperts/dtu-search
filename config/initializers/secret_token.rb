987136829a5485bc382310793c841c6969463526
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-01-05 10:36:39 -0600

    Skeleton rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dtu::Application.config.secret_token = '2d288200df88b1768c17fcae156ea6c2076706a35a25b5863c2b6fd0c12901fb6f997a58a3a3ed6ade233287125bb1ff3729524c75ade2a9694f643cb3997625'
