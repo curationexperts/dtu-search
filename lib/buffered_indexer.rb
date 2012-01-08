f7985d72f3c8f3593fed2b24bb7f3c25ad013fd5
MediaShelf User <mediashelf@spakinth.cvt.dk>
2012-01-08 05:33:29 +0100

    Add a batch of documents

--- lib/buffered_indexer.rb
+++ lib/buffered_indexer.rb
@@ -1,17 +1,22 @@
 class BufferedIndexer 
-  BUFFER_SIZE=10000
+  BUFFER_SIZE=1000
+
+
 
   def initialize
     @count = 0
+    @docs = []
   end
 
   def flush
+    solr.add @docs
     solr.commit
+    @docs = []
     @count = 0
   end
 
   def add(doc)
-    solr.add doc
+    @docs << doc
     increment
   end
 
