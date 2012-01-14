15f68b11c035dffd603037188eb3cdc0d08de365
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-01-13 20:20:10 -0600

    Rearrange the encoder to not fail until we got as much done as we cna

--- lib/dtu/article_encoder.rb
+++ lib/dtu/article_encoder.rb
@@ -5,12 +5,12 @@ module DTU
       doc['id'] = doc['localinfo_key_t'].first
       doc['metastore_id_s'] = id
       doc['format'] = 'article'
+      doc['author_name_facet'] = doc['author_name_t']
+      doc['title_s'] = doc['article_title_t'].first
       if doc['journal_title_t'].nil?
         raise "No Journal title for article #{doc['id']}"
       end
       doc['journal_title_facet'] = doc['journal_title_t'].first
-      doc['author_name_facet'] = doc['author_name_t']
-      doc['title_s'] = doc['article_title_t'].first
     end
   end
 end
