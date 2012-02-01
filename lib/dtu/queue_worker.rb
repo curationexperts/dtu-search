639d282cc63ba8b74c9d9850f9e9fd569cd3d501
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-02-01 10:02:37 -0600

    Catch StandardError, not Exception

--- lib/dtu/queue_worker.rb
+++ lib/dtu/queue_worker.rb
@@ -7,16 +7,17 @@ module DTU
       @q = Carrot.queue('search.index')
 
       @buff = DTU::ShardedIndexer.new
-      Signal.trap("INT") { shutdown_cleanly }
-      Signal.trap("TERM") { shutdown_cleanly }
-      Signal.trap("KILL") { shutdown_cleanly }
+      at_exit { shutdown_cleanly} 
+      # Signal.trap("INT") { shutdown_cleanly }
+      # Signal.trap("TERM") { shutdown_cleanly }
+      # Signal.trap("KILL") { shutdown_cleanly }
     end
 
     def shutdown_cleanly
       print "Finishing writes..."
       @buff.flush()
       puts "Done."
-      exit!
+     # Thread.exit
     end
 
 
@@ -31,14 +32,12 @@ module DTU
           begin
             doc = DTU::ArticleEncoder.solrize(m.id, m.xml)
             @buff.add(doc)
-          rescue SystemExit, Interrupt, SignalException
-            raise
           rescue RSolr::Error::Http, Errno::ECONNREFUSED => exception
             puts "Fatal #{exception.class}, exception see log"
             logger.fatal( "\n\n#{exception.class} (#{exception.message})\n\n")
             logger.flush
             exit!
-          rescue Exception => exception
+          rescue StandardError => exception
             puts "Caught #{exception.class}, see log"
             logger.fatal( "\n\n#{exception.class} (#{exception.message}):\n    " + exception.backtrace.join("\n    ") + "\n\n")
             logger.flush
