8eb103bd6d79ec29c484e79b70d7426c6985b7b9
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-02-03 15:16:33 -0600

    Remove conditional for loading compass

--- config/initializers/sass.rb
+++ config/initializers/sass.rb
@@ -1,6 +1,4 @@
 Rails.configuration.sass.tap do |config|
-  if Rails.env.development? || Rails.env.test?
 	  require 'compass'
 	  config.load_paths << "#{Gem.loaded_specs['compass'].full_gem_path}/frameworks/compass/stylesheets"  
-  end
 end
