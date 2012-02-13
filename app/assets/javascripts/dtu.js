136cd79a4d89fb7c764dfcbb267fef73e872a68f
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-02-13 15:48:45 -0600

    expand/collapse details

--- app/assets/javascripts/dtu.js
+++ app/assets/javascripts/dtu.js
@@ -1,6 +1,16 @@
 
 jQuery(function($){
   function redirect(url){return"http://redirect.cvt.dk/?url="+encodeURIComponent(url) }
+
+  $(".detailed-view").click(function() {
+    if ($(this).hasClass('collapsed')) {
+      $(this).removeClass('collapsed');
+    } else {
+      $(this).addClass('collapsed');
+    }
+		$(this).next().toggle();
+	}).click()
+  
 });
 
 
