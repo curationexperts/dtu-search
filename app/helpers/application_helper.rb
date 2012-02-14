3c1fc675635b2a6aa9489020349c4c52f1713eb4
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2012-02-14 15:45:20 -0600

    1992: generate links to local fulltext if available

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,15 +4,28 @@ module ApplicationHelper
   end
 
   def link_to_fulltext(document)
-    title = CGI.escape document['title_t'].first if document['title_t']
-    doi = CGI.escape(document['recordid_doi_t'].first) if document['recordid_doi_t'] 
-    author = CGI.escape(document['author_name_t'].first) if document['author_name_t'] 
-    issue = document['journal_issue_t'].first if document['journal_issue_t']
-    year = document['journal_year_t'].first if document['journal_year_t']
-    vol = document['journal_vol_t'].first if document['journal_vol_t']
-    page = document['journal_page_t'].first if document['journal_page_t']
-    issn = document['journal_issn_t'].first if document['journal_issn_t']
-    "http://sfx.cvt.dk/sfx_local?rft.jtitle=#{title}&amp;rft.issn=#{issn}&amp;rft.pages=#{page}&amp;rft.spage=#{page}&amp;rft.volume=#{vol}&amp;rft.issue=#{issue}&amp;rft.date=#{year}&amp;rft.atitle=#{title}&amp;rft.doi=#{ doi }&amp;rft.au=#{author}&amp;rfr_id=info:sid/dlib.dtu.dk:DTUDigitalLibrary".html_safe
+    # grab the info for generating local urls if it's in the document
+    document_source = document.fetch("localinfo_source_t", []).first  # case sensitive field name!  localinfo_source_t NOT localInfo_source_t
+    fulltext_local_path = document.fetch("fulltext_local_t", []).first 
+    key = document.fetch("localinfo_systemno_t", []).first # sf:art/sf:localinfo/sf:systemno
+    
+    # Generate SFX link unless all of the info for generating local urls is available
+    if document_source.nil? || fulltext_local_path.nil? || key.nil?
+      title = CGI.escape document['title_t'].first if document['title_t']
+      doi = CGI.escape(document['recordid_doi_t'].first) if document['recordid_doi_t'] 
+      author = CGI.escape(document['author_name_t'].first) if document['author_name_t'] 
+      issue = document['journal_issue_t'].first if document['journal_issue_t']
+      year = document['journal_year_t'].first if document['journal_year_t']
+      vol = document['journal_vol_t'].first if document['journal_vol_t']
+      page = document['journal_page_t'].first if document['journal_page_t']
+      issn = document['journal_issn_t'].first if document['journal_issn_t']
+      return "http://sfx.cvt.dk/sfx_local?rft.jtitle=#{title}&amp;rft.issn=#{issn}&amp;rft.pages=#{page}&amp;rft.spage=#{page}&amp;rft.volume=#{vol}&amp;rft.issue=#{issue}&amp;rft.date=#{year}&amp;rft.atitle=#{title}&amp;rft.doi=#{ doi }&amp;rft.au=#{author}&amp;rfr_id=info:sid/dlib.dtu.dk:DTUDigitalLibrary".html_safe
+    else
+      pi = fulltext_local_path[document_source.length+1..fulltext_local_path.length]
+      referrer_id = "info:sid/dlib.dtu.dk:DTUDigitalLibraryBlacklight" # this is the identifier the running application uses. Maybe Blacklight application should use something slightly different.
+      redirect_query_url = "http://dtu-ftc.cvt.dk/cgi-bin/fulltext/#{document_source}?pi=#{pi}&key=#{key}&rfr_id=#{referrer_id}"
+      return "http://redirect.cvt.dk?url=#{CGI.escape(redirect_query_url)}".html_safe
+    end
   end
 
   def show_authors(document)
---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe ApplicationHelper do
+
+  describe "link_to_fulltext"  do
+    it "should default to generating sfx links" do
+      doc = DTU::ArticleEncoder.solrize('article_without_fulltext', File.open("spec/fixtures/first_article.xml"))
+      fulltext_url = helper.link_to_fulltext(doc)
+      CGI.unescape(fulltext_url).should == "http://sfx.cvt.dk/sfx_local?rft.jtitle=Short-term effects of planktonic rotifers and cladocerans on phytoplankton of the River Nile&amp;rft.issn=00207233&amp;rft.pages=403&amp;rft.spage=403&amp;rft.volume=61&amp;rft.issue=4&amp;rft.date=2004-2008&amp;rft.atitle=Short-term effects of planktonic rotifers and cladocerans on phytoplankton of the River Nile&amp;rft.doi=&amp;rft.au=Khalifa, Nehad&amp;rfr_id=info:sid/dlib.dtu.dk:DTUDigitalLibrary"
+    end
+    it "should generate local urls if info for local fulltext is available" do
+      doc = DTU::ArticleEncoder.solrize('article_with_fulltext', File.open("spec/fixtures/article_with_fulltext.xml"))
+      fulltext_url = helper.link_to_fulltext(doc) 
+      CGI.unescape(fulltext_url).should == "http://redirect.cvt.dk?url=http://dtu-ftc.cvt.dk/cgi-bin/fulltext/elsevier?pi=/0017/9310/00520013/09000763.pdf&key=130140113&rfr_id=info:sid/dlib.dtu.dk:DTUDigitalLibraryBlacklight"
+    end
+  end
+end
+
