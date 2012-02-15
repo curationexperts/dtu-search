1008acf7e47c775222b160199ddb9b009458a5c9
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-02-15 14:36:55 -0600

    correct way to set tablename

--- app/models/metastore.rb
+++ app/models/metastore.rb
@@ -1,5 +1,5 @@
 class Metastore < ActiveRecord::Base
-  primary_key = 'id' #required for jruby bug -- http://jira.codehaus.org/browse/JRUBY-6358
-  table_name = 'public.metastore'
+  self.primary_key = 'id' #required for jruby bug -- http://jira.codehaus.org/browse/JRUBY-6358
+  self.table_name = 'public.metastore'
 
 end
