d91f94628f840269346c4c1c8ef36a7548aee661
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-03-07 13:16:54 -0600

    Links for BX integration

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,15 @@
 module ApplicationHelper
+  def link_to_recommendations(document)
+      title = CGI.escape document['title_t'].first if document['title_t']
+      doi = CGI.escape(document['recordid_doi_t'].first) if document['recordid_doi_t'] 
+      author = CGI.escape(document['author_name_t'].first) if document['author_name_t'] 
+      issue = document['journal_issue_t'].first if document['journal_issue_t']
+      year = document['journal_year_t'].first if document['journal_year_t']
+      vol = document['journal_vol_t'].first if document['journal_vol_t']
+      page = document['journal_page_t'].first if document['journal_page_t']
+      issn = document['journal_issn_t'].first if document['journal_issn_t']
+      return "http://digitallibrary.dtu.dk/BXProxy/recommend?rft.jtitle=#{title}&amp;rft.issn=#{issn}&amp;rft.pages=#{page}&amp;rft.spage=#{page}&amp;rft.volume=#{vol}&amp;rft.issue=#{issue}&amp;rft.date=#{year}&amp;rft.atitle=#{title}&amp;rft.doi=#{ doi }&amp;rft.au=#{author}&amp;rfr_id=info:sid/dlib.dtu.dk:DTUDigitalLibrary".html_safe
+  end
   def link_to_fulltext(document)
     # grab the info for generating local urls if it's in the document
     document_source = document.fetch("localinfo_source_t", []).first  # case sensitive field name!  localinfo_source_t NOT localInfo_source_t
